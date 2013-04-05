3ca2ebdda4d6699d30d60d34746eca927042bb5d
Jin Zhang <jinzhang273@gmail.com>
2013-04-05 15:44:17 -0700

    added single_post and pagination

--- jin_blog/urls.py
+++ jin_blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r"^$", "jin_blog.views.home", name="home"),
     url(r"^contact/$", "jin_blog.views.contact", name="contact"),
     url(r"^about/$", "jin_blog.views.about", name="about"),
+    url(r"^blog/(?P<pk>\d+)/$", "jin_blog.views.single_post", name="single_post"),
     url(r'^admin/', include(admin.site.urls)),
 )
--- jin_blog/views.py
+++ jin_blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from jin_blog.models import Post, Page
 
 def home(request):
-    posts = Post.objects.filter(published=True)
+    post_list = Post.objects.filter(published=True)
+    # Show 5 posts per page
+    paginator = Paginator(post_list, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
     return render_to_response("home.html", dict(posts=posts))
 
 def about(request):
@@ -12,3 +25,7 @@ def about(request):
 def contact(request):
     page = Page.objects.get(title="Contact")
     return render_to_response("contact.html", dict(page=page))
+
+def single_post(request, pk):
+    post = get_object_or_404(Post, pk=pk, published=True)
+    return render_to_response("single_post.html", dict(post=post))
