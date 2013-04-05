55985db26cebec717a1bb72db70ae5717f3b8e99
Jin Zhang <jinzhang273@gmail.com>
2013-04-05 03:03:13 -0700

    added about and contact pages

--- jin_blog/urls.py
+++ jin_blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'jin_blog.views.home', name='home'),
     # url(r'^jin_blog/', include('jin_blog.foo.urls')),
     url(r"^$", "jin_blog.views.home", name="home"),
-
+    url(r"^contact/$", "jin_blog.views.contact", name="contact"),
+    url(r"^about/$", "jin_blog.views.about", name="about"),
     url(r'^admin/', include(admin.site.urls)),
 )
--- jin_blog/views.py
+++ jin_blog/views.py
@@ -3,5 +3,10 @@ from jin_blog.models import Post
 
 def home(request):
     posts = Post.objects.filter(published=True)
-
     return render_to_response("home.html", dict(posts=posts))
+
+def about(request):
+    return render_to_response("about.html")
+
+def contact(request):
+    return render_to_response("contact.html")
