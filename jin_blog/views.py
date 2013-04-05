101cccb655d5d8683c1b480f638e6332932a622f
Jin Zhang <jinzhang273@gmail.com>
2013-04-05 16:10:54 -0700

    added 404 handler

--- jin_blog/views.py
+++ jin_blog/views.py
@@ -19,11 +19,11 @@ def home(request):
     return render_to_response("home.html", dict(posts=posts))
 
 def about(request):
-    page = Page.objects.get(title="About")
+    page = get_object_or_404(Page, title="About")
     return render_to_response("about.html", dict(page=page))
 
 def contact(request):
-    page = Page.objects.get(title="Contact")
+    page = get_object_or_404(Page, title="Contact")
     return render_to_response("contact.html", dict(page=page))
 
 def single_post(request, pk):
