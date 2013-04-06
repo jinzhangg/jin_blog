814c13a054a2c9bf53966ebd8f28c6b7aba8b3d7
Jin Zhang <jinzhang273@gmail.com>
2013-04-06 06:06:43 -0700

    changed settings

--- jin_blog/settings.py
+++ jin_blog/settings.py
@@ -55,7 +55,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -66,7 +66,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -77,7 +77,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -167,10 +166,10 @@ LOGGING = {
 DATABASES['default'] = dj_database_url.config()
 
 try:
-    from jin_blog.local_settings import *
+    from jin_blog.prod_settings import *
 except ImportError:
     pass
 try:
-    from jin_blog.prod_settings import *
+    from jin_blog.local_settings import *
 except ImportError:
     pass
